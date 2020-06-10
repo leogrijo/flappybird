3338532c8efab689bef4f9560e668d8831714191
leogrijo <leonardogrijo@gmail.com>
2020-06-10 04:31:58 -0300

    Adicionando estato e transição das telas

--- jogo.js
+++ jogo.js
@@ -33,7 +33,7 @@ const flappyBird = {
         flappyBird.localRenderX,    flappyBird.localRenderY, // Coordenadas do local da tela onde irá ser desenhada a imagem
         flappyBird.largura,         flappyBird.altura // Tamanho do que será desenhado na tela - No caso, mesmo tamanho do recorte da imagem
       );
-    }
+    } 
 };
 
 //Objeto que representa o Chão
@@ -100,27 +100,141 @@ const planoDefundo = {
   }
 };
 
+
+//Objeto que representa a imagem de "Get Ready" na tela inicial do jogo
+const imgGetReady = {
+  xInicialRecorte: 134, //Coordenada X de onde o recorte começa no arquivo de origem
+  yInicialRecorte: 0, //Coordenada Y de onde o recorte começa no arquivo de origem
+  xFinalRecorte: 174,  //Coordenada X de onde o recorte termina no arquivo de origem
+  yFinalRecorte: 152,  //Coordenada Y de onde o recorte termina no arquivo de origem
+  localRenderX: (canvas.width / 2) - 174 / 2,   //Coordenada X de onde a imagem será renderizada na tela
+  localRenderY: 50,  //Coordenada Y de onde a imagem será renderizada na tela - Expressão para que a imagem apareça centralizada
+  largura: 174,        //Largura que a imagem irá aparecer na tela
+  altura: 152,         //Altura que a imagem irá aparecer na tela
+  desenha(){          //Função para desenhar a imagem na tela
+    
+    context.drawImage(
+      imagemFonte, // Arquivo de origem de onde a imagem que será projetada na tela será recortada
+      imgGetReady.xInicialRecorte, imgGetReady.yInicialRecorte, // Coordenadas para desenhar a imagem - Canto superior esquerdo do que será desenhado
+      imgGetReady.xFinalRecorte,   imgGetReady.yFinalRecorte, // Medidas da largura e altura da imagem - Coordenada do canto inferior direito da imagem
+      imgGetReady.localRenderX,    imgGetReady.localRenderY, // Coordenadas do local da tela onde irá ser desenhada a imagem
+      imgGetReady.largura,         imgGetReady.altura // Tamanho do que será desenhado na tela - No caso, mesmo tamanho do recorte da imagem - Opcional. Função permite que esses parâmetros sejam suprimidos
+    );
+
+    
+  }
+};
+
+//Objeto que representa a imagem da tela de game over
+const imgGameOver = {
+  xInicialRecorte: 134, //Coordenada X de onde o recorte começa no arquivo de origem
+  yInicialRecorte: 152, //Coordenada Y de onde o recorte começa no arquivo de origem
+  xFinalRecorte: 226,  //Coordenada X de onde o recorte termina no arquivo de origem
+  yFinalRecorte: 201,  //Coordenada Y de onde o recorte termina no arquivo de origem
+  localRenderX: (canvas.width / 2) - 226 / 2,   //Coordenada X de onde a imagem será renderizada na tela
+  localRenderY: 50,  //Coordenada Y de onde a imagem será renderizada na tela - Expressão para que a imagem apareça centralizada
+  largura: 226,        //Largura que a imagem irá aparecer na tela
+  altura: 201,         //Altura que a imagem irá aparecer na tela
+  desenha(){          //Função para desenhar a imagem na tela
+    
+    context.drawImage(
+      imagemFonte, // Arquivo de origem de onde a imagem que será projetada na tela será recortada
+      imgGameOver.xInicialRecorte, imgGameOver.yInicialRecorte, // Coordenadas para desenhar a imagem - Canto superior esquerdo do que será desenhado
+      imgGameOver.xFinalRecorte,   imgGameOver.yFinalRecorte, // Medidas da largura e altura da imagem - Coordenada do canto inferior direito da imagem
+      imgGameOver.localRenderX,    imgGameOver.localRenderY, // Coordenadas do local da tela onde irá ser desenhada a imagem
+      imgGameOver.largura,         imgGameOver.altura // Tamanho do que será desenhado na tela - No caso, mesmo tamanho do recorte da imagem - Opcional. Função permite que esses parâmetros sejam suprimidos
+    );
+
+    
+  }
+};
+
+//TELA ATIVA - Estado corrente do jogo
+let telaAtiva = {}; // Guarda a tela atualmente ativa
+function ativarTela(tela){
+  telaAtiva = tela;
+};
+
+// TELAS - Separando a estrutura das telas em tela inicial, tela do jogo e tela de game over
+const telas ={
+  GETREADY: {
+    desenha(){
+      //A ordem das chamadas das funções de desenho afetam a ordem das imagens na tela
+      //Chamada da função para desenhar o plano de fundo
+      planoDefundo.desenha();
+      
+      //Chamada da função para desenhar o chão na tela
+      chao.desenha();
+
+      //Chamada da função para desenhar a imagem de "Get Ready" na tela
+      imgGetReady.desenha();
+      
+      //Chamada da função para desenhar o Flappy Bird na tela
+      flappyBird.desenha();
+    },
+    click(){
+      ativarTela(telas.JOGO);
+    },
+    atualiza(){
+      //Chamada da função para criar o efeito de movimentação o Flappy Bird na tela
+      //flappyBird.atualiza();
+    }
+  },
+  JOGO: {
+    desenha(){
+      //A ordem das chamadas das funções de desenho afetam a ordem das imagens na tela
+      //Chamada da função para desenhar o plano de fundo
+      planoDefundo.desenha();
+      
+      //Chamada da função para desenhar o chão na tela
+      chao.desenha();
+
+      //Chamada da função para desenhar o Flappy Bird na tela
+      flappyBird.desenha();
+    },
+    atualiza(){
+      //Chamada da função para criar o efeito de movimentação o Flappy Bird na tela
+      flappyBird.atualiza();
+    }
+  },
+  GAMEOVER: {
+    desenha(){
+      //Chamada da função para desenhar o plano de fundo
+      planoDefundo.desenha();
+      
+      //Chamada da função para desenhar o chão na tela
+      chao.desenha();
+
+      //Chamada da função para desenhar a imagem de "Get Ready" na tela
+      imgGameOver.desenha();
+      
+      //Chamada da função para desenhar o Flappy Bird na tela
+      flappyBird.desenha();
+    },
+    atualiza(){
+      //Chamada da função para criar o efeito de movimentação o Flappy Bird na tela
+      flappyBird.atualiza();
+    }
+  }
+};
+
 //Função para fazer um loop do desenho na tela - Conceito de FPS vem daí - A imagem é desenhada na tela de forma repetida
 function loop(){
-  //A ordem das chamadas das funções de desenho afetam a ordem das imagens na tela
-  //Chamada da função para desenhar o plano de fundo
-  planoDefundo.desenha();
-  
-  //Chamada da função para desenhar o chão na tela
-  chao.desenha();
+  telaAtiva.atualiza();
+  telaAtiva.desenha();
 
-  //Chamada da função para criar o efeito de movimentação o Flappy Bird na tela
-  flappyBird.atualiza();
-  
-  //Chamada da função para desenhar o Flappy Bird na tela
-  flappyBird.desenha();
-
-  
-  
-
-  //Função para otimizar a forma como a animação é gerada na tela - gera a animação de forma mais suave na tela
+    //Função para otimizar a forma como a animação é gerada na tela - gera a animação de forma mais suave na tela
   requestAnimationFrame(loop);
 
 };
+
+ativarTela(telas.GETREADY); //Ativando a tela de início do jogo. Ponto de partida padrão sempre que a página for atualizada
+
+//Adicionando um listener para detectar clicks na tela. Caso haja algum, ativa a tela de jogo
+window.addEventListener('click', function(){
+  if (telaAtiva.click) { // Caso haja click em algum lugar da tela, chama a função click da telaAtiva
+    telaAtiva.click();
+  }
+});
 
 loop();
